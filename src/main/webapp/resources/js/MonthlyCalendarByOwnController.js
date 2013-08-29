a5a9edcf8a47452d8e54474d5a1bc51b9a1fea7c
Esther <Esther@Esther-PC>
2013-08-30 00:59:00 +0200

    Reparando pérdidas 2

--- src/main/webapp/resources/js/MonthlyCalendarByOwnController.js
+++ src/main/webapp/resources/js/MonthlyCalendarByOwnController.js
@@ -1,37 +1,41 @@
 
-var MonthlyCalendarByOwn = function(year, month){
-    MyCalendar.call(this, MonthlyCalendarByOwn.events , year, month, null, MonthlyCalendarByOwn.path);    
-    $('#calendar').fullCalendar('option','aspectRatio', 16/6);
+var MonthlyCalendarByOwn = function(year, month) {
+    MyCalendar.call(this, MonthlyCalendarByOwn.events, year, month, null, MonthlyCalendarByOwn.path);
+    $('#calendar').fullCalendar('option', 'aspectRatio', 16 / 6);
     window.addEventListener("popstate", function(e) {
         var historyObject = e.state;
-        $('#calendar').fullCalendar('gotoDate', historyObject.year, historyObject.month-1);
+        $('#calendar').fullCalendar('changeView', historyObject.view);
+        $('#calendar').fullCalendar('gotoDate',
+                historyObject.date.getFullYear(),
+                historyObject.date.getMonth(),
+                historyObject.date.getDate());
     });
 };
 
 /*Extends of Calendar*/
 MonthlyCalendarByOwn.prototype = Object.create(MyCalendar.prototype);
 
-MonthlyCalendarByOwn.path={"monthly": applicationPath+'reservations/month/'};
+MonthlyCalendarByOwn.path = {"monthly": applicationPath + 'reservations/month/'};
 
 
-MonthlyCalendarByOwn.events = function(start, end, callback){
-    
-    start.setDate(start.getDate()+7);
+MonthlyCalendarByOwn.events = function(start, end, callback) {
+
+    start.setDate(start.getDate() + 7);
     var year = start.getFullYear();
-    var month = start.getMonth()+1;
-    var label = MyCalendar.monthNames[locale][month-1]+' '+year;
-    
+    var month = start.getMonth() + 1;
+    var label = MyCalendar.monthNames[locale][month - 1] + ' ' + year;
+
     //On ajax success
-    var success = function(response){
+    var success = function(response) {
         $('#reservation_nav .label').text(label);
         var events = MonthlyCalendarByOwn.serializeToCalendarEvents(response);
-        
-        
-        var historyObject={"year": year, "month": month, "events": events};
+
+
+        var historyObject = {"year": year, "month": month, "events": events};
         MyCalendar.updateLinks(MonthlyCalendarByOwn.path);
         callback(events);
     };
-    
+
     //Ajax Request
     Reservation.getOwnReservationsByMonth(year, month, success, ajaxError);
 };
@@ -50,9 +54,9 @@ MonthlyCalendarByOwn.serializeToCalendarEvents = function(instances) {
             end: new Date(instance.endTimeDate),
             url: applicationPath + 'reservations/' + instance.reservation.id
         };
-        
-        event.backgroundColor = (loggedUniqueName == instance.reservation.owner.uniqueName)? 
-            MyCalendar.orange : event.backgroundColor = MyCalendar.grey;
+
+        event.backgroundColor = (loggedUniqueName == instance.reservation.owner.uniqueName) ?
+                MyCalendar.orange : event.backgroundColor = MyCalendar.grey;
 
         events.push(event);
     }
